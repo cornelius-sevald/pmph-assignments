ec3b404828b298865523db633ecb8a5a3c6f8f1f
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2022-09-14 20:07:24 +0200

    [A1] Task 3 add serial map function

--- wa1/w1-code-handin/task3/wa1-task3.cu
+++ wa1/w1-code-handin/task3/wa1-task3.cu
@@ -2,15 +2,38 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main(int argc, char** argv) {
-    // Size of array
-    // Can be set by command line args but defaults to 753411
-    unsigned long N = 753411;
+void serial_map(float *in, float *out, unsigned int N) {
+    for (unsigned int i = 0; i < N; ++i) {
+        float x = in[i];
+        float y = (x/(x-2.3))*(x/(x-2.3))*(x/(x-2.3)); // (x/(x-2.3))^3
+        out[i] = y;
+    }
+}
 
+int main(int argc, char** argv) {
+    // size of array
+    // can be set by command line args but defaults to 753411
+    unsigned int N = 753411;
     if (argc > 1) {
         N = strtoul(argv[1], NULL, 10);
         assert(N != 0);
     }
+    size_t mem_size = N*sizeof(float);
 
-    printf("N = %lu\n", N);
+    // allocate host memory
+    float *h_in  = (float *) malloc(mem_size);
+    float *h_out = (float *) malloc(mem_size);
+
+    // initialize memory
+    for (unsigned int i = 0; i < N; ++i) {
+        h_in[i] = (float) (i+1);
+    }
+
+    // preform serial map
+    serial_map(h_in, h_out, N);
+
+    // print results for debugging
+    for (unsigned int i = 0; i < N; ++i) {
+        printf("%d\t%.2f\n", i, h_out[i]);
+    }
 }
