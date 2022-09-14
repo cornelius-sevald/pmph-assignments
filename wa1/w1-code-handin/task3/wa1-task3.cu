2b37b1b2e26658751636eb8e8338eac7a34bff6e
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2022-09-14 20:34:58 +0200

    [A1] Task 3 add parallel map

--- wa1/w1-code-handin/task3/wa1-task3.cu
+++ wa1/w1-code-handin/task3/wa1-task3.cu
@@ -10,6 +10,16 @@ void serial_map(float *in, float *out, unsigned int N) {
     }
 }
 
+__global__ void parallel_map(float *d_in, float *d_out, unsigned int N) {
+    const unsigned int lid = threadIdx.x;
+    const unsigned int gid = blockIdx.x*blockDim.x + lid;
+    if (gid < N) {
+        float x = d_in[gid];
+        float y = (x/(x-2.3))*(x/(x-2.3))*(x/(x-2.3)); // (x/(x-2.3))^3
+        d_out[gid] = y;
+    }
+}
+
 int main(int argc, char** argv) {
     // size of array
     // can be set by command line args but defaults to 753411
@@ -19,6 +29,8 @@ int main(int argc, char** argv) {
         assert(N != 0);
     }
     size_t mem_size = N*sizeof(float);
+    unsigned int block_size = 256;
+    unsigned int num_blocks = ((N + (block_size - 1)) / block_size);
 
     // allocate host memory
     float *h_in  = (float *) malloc(mem_size);
@@ -29,11 +41,29 @@ int main(int argc, char** argv) {
         h_in[i] = (float) (i+1);
     }
 
-    // preform serial map
-    serial_map(h_in, h_out, N);
+    // allocate device memory
+    float *d_in;
+    float *d_out;
+    cudaMalloc((void **) &d_in,  mem_size);
+    cudaMalloc((void **) &d_out, mem_size);
+
+    // copy host memory to device
+    cudaMemcpy(d_in, h_in, mem_size, cudaMemcpyHostToDevice);
+
+    // preform parallel map
+    parallel_map<<<num_blocks, block_size>>>(d_in, d_out, N);
+
+    // copy host memory to device
+    cudaMemcpy(h_out, d_out, mem_size, cudaMemcpyDeviceToHost);
 
     // print results for debugging
     for (unsigned int i = 0; i < N; ++i) {
         printf("%d\t%.2f\n", i, h_out[i]);
     }
+
+    // clean up
+    free(h_in);
+    free(h_out);
+    cudaFree(d_in);
+    cudaFree(d_out);
 }
