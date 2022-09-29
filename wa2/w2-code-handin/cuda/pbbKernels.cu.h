81cb516c6e1e622f8e39a261fe69c93be5c44b25
Cornelius Sevald-Krause <cornelius.sevald@protonmail.com>
2022-09-29 07:05:33 +0200

    [A2] Task 4 implementation

--- wa2/w2-code-handin/cuda/pbbKernels.cu.h
+++ wa2/w2-code-handin/cuda/pbbKernels.cu.h
@@ -227,7 +227,14 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const unsigned int idx) {
     //   the first warp. This works because
     //   warp size = 32, and 
     //   max block size = 32^2 = 1024
-    if (lane == (WARP-1)) { ptr[warpid] = OP::remVolatile(ptr[idx]); } 
+    if (lane == (WARP-1)) {
+        // First read value and save in temporary variable
+        typename OP::RedElTp tmp = OP::remVolatile(ptr[idx]);
+        // Sync threads
+        __syncthreads();
+        // Then write temporary value to first warp
+        ptr[warpid] = tmp;
+    } 
     __syncthreads();
 
     // 3. scan again the first warp
